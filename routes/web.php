9ec24e258de04842a9140c87ed1e143cef4908ec
ReservaDoPretti <reservadopretti@outlook.com>
2025-08-14 15:43:20 -0300

    Crud Ambientes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Ambiente\AmbienteCreate;
+use App\Livewire\Ambiente\AmbienteEdit;
+use App\Livewire\Ambiente\AmbienteList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
+Route::get('/ambiente/{id}/edit', AmbienteEdit::class)->name('ambiente.edit');
+Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
