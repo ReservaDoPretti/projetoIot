c2988382b9d4daf692f38886d59e05bf34983bdd
SmurfDoPretti <reservadopretti@outlook.com>
2025-08-19 13:31:49 -0300

    Revert "Crud Ambientes"

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Livewire\Ambiente\AmbienteCreate;
-use App\Livewire\Ambiente\AmbienteEdit;
-use App\Livewire\Ambiente\AmbienteList;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
-Route::get('/ambiente/{id}/edit', AmbienteEdit::class)->name('ambiente.edit');
-Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
+Route::get('/', function () {
+    return view('welcome');
+});
