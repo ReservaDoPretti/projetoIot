78f3e1376ca038fd735b03abd9af0054ae995dd9
ReservaDoPretti <reservadopretti@outlook.com>
2025-10-07 14:07:21 -0300

    Ambiente em produção, resolvido o problema de ter perdido algumas funcionalidades

---
+++ resources/views/livewire/ambiente/ambiente-list.blade.php
@@ -0,0 +1,92 @@
+<div class="container mt-4">
+        
+
+    <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-center text-white">
+                <h2 class="text-dark d-flex flex-row justify-content-start mb-2"> Ambiente <i class="bi bi-tree-fill"></i></h2>
+
+               
+            </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="text" wire:model.live="search" class="form-control" wire:model.live="search"
+                        placeholder="Buscar Sensores..." class="btn btn-primary">
+                </div>
+
+                <div class="col-md-3">
+                    <select wire:model="perPage" class="form-select">
+                        <option value="10">10 por página</option>
+                        <option value="25">25 por página</option>
+                        <option value="50">50 por página</option>
+                        <option value="100">100 por página</option>
+                    </select>
+                </div>
+            </div> 
+
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+            @endif
+
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Ambiente_ID</th>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($ambientes as $ambiente)
+                            <tr>
+                                <td>{{ $ambiente->id }}</td>
+                                <td>{{ $ambiente->nome }}</td>
+                                <td>{{ $ambiente->descricao }}</td>
+                                <td>{{ $ambiente->status }}</td>
+                              <td>
+                                    <a href="{{ route('ambiente.edit', $ambiente->id) }}" class="btn btn-sm btn-warning">
+                                        <i class="bi bi-pencil"></i>
+                                    </a>
+                                    <button wire:click="delete({{ $ambiente->id }})"
+                                            class="btn btn-sm btn-danger"wire:confirm="Tem certeza que deseja excluir">
+                                            <i class="bi bi-trash"></i>
+                                     </button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="text-center">Nenhum Ambiente encontrado.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="mt-3">
+                {{ $ambientes->links() }}
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\Ambiente\AmbienteCreate;
+use App\Livewire\Ambiente\AmbienteEdit;
+use App\Livewire\Ambiente\AmbienteList;
 use App\Livewire\Dashboard;
 
 use App\Livewire\Registro\RegistroList;
@@ -11,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
 
+Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
+Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
+Route::get('/ambiente/{id}/edit', AmbienteEdit::class)->name('ambiente.edit');
 Route::get('/registro', RegistroList::class)->name('registro.list');
 Route::get('/sensor/create', SensorCreate::class)->name('sensor.create');
 Route::get('/sensor/list', SensorList::class)->name('sensor.list');
